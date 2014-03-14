c335fbe4e7499bed1074ae7bff971fd569c98824
brian1987 <pisano.b@gmail.com>
2014-03-14 11:07:14 -0400

    first commit in demo rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitDemoRepo::Application.config.secret_key_base = '8f6dd47d0a699ce210f2f9053e0a326b383c06f7dd491bd46449ed1940e39fc9ce54929daa12146fdb1288131359c8149bc7e26e1475975a48a1ee7b99ec8d06'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GitDemoRepo::Application.config.session_store :cookie_store, key: '_git-demo-repo_session'
